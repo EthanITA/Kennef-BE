63320e914c17f823a408e60f8ffd558b0aab3eb1
madong <viaimperiale@gmail.com>
2023-09-30 13:23:40 +0200

    feat: add PayPal service

---
+++ src/services/PayPal.ts
@@ -0,0 +1,64 @@
+import * as PP from 'paypal-rest-sdk';
+import { Item, Payment, PaymentResponse } from 'paypal-rest-sdk';
+import { sum, toNumber } from 'lodash';
+
+type Currency = 'EUR' | string;
+
+PP.configure({
+  mode: process.env.NODE_ENV === 'development' ? 'sandbox' : 'live',
+  client_id: process.env.PP_CLIENT_ID,
+  client_secret: process.env.PP_SECRET_KEY,
+});
+
+class PayPal {
+  currency: Currency;
+
+  constructor() {
+    this.currency = 'EUR';
+  }
+
+  static async getPaymentResponse(items: Item[]): Promise<string | undefined | never> {
+    const paypal = new this();
+    const payment = paypal.createPayment(items);
+    const paymentResponse = await paypal.createSession(payment);
+    return paypal.getLink(paymentResponse);
+  }
+
+  // Function to create PayPal payment
+  createPayment(items: Item[]): Payment {
+    const totalAmount = sum(items.map((item) => toNumber(item.price)));
+
+    return {
+      intent: 'sale',
+      payer: {
+        payment_method: 'paypal',
+      },
+      transactions: [
+        {
+          amount: {
+            total: totalAmount.toFixed(2),
+            currency: this.currency,
+          },
+          item_list: {
+            items,
+          },
+        },
+      ],
+      redirect_urls: {
+        return_url: `${process.env.APP_URL}`,
+        cancel_url: `${process.env.APP_URL}`,
+      },
+    };
+  }
+
+  // Function to create PayPal session
+  createSession(payment: Payment): Promise<PaymentResponse | never> {
+    return new Promise<PaymentResponse>((resolve, reject) => PP.payment.create(payment, (err, res) => (err ? reject(err) : resolve(res))));
+  }
+
+  getLink(paymentResponse: PaymentResponse): string | undefined {
+    return paymentResponse.links?.find?.((link) => link.rel === 'approval_url')?.href;
+  }
+}
+
+export default PayPal;
