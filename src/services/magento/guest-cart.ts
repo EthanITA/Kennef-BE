a4c1606c087cd6419dfca9af745a8ca70603a259
madong <viaimperiale@gmail.com>
2023-09-30 14:55:47 +0200

    refactor: add PayableCart

--- src/services/magento/guest-cart.ts
+++ src/services/magento/guest-cart.ts
@@ -1,27 +1,9 @@
-import Magento from './index';
-import { Cart, CartTotal } from '../../types/cart';
+import { Cart } from '../../types/cart';
+import PayableCart from './payable-cart';
 
-class GuestCart extends Magento {
-  private cartId: Cart['id'];
-  public cart?: Cart;
-
+class GuestCart extends PayableCart {
   constructor(cartId: Cart['id']) {
-    super();
-    this.cartId = cartId;
-  }
-
-  async getCart(): Promise<Cart | undefined> {
-    return this.axios
-      .get<Cart>(`guest-carts/${this.cartId}`)
-      .then((res) => (this.cart = res.data))
-      .catch(() => (this.cart = undefined));
-  }
-
-  async getTotal(): Promise<CartTotal | undefined> {
-    return this.axios
-      .get<CartTotal>(`guest-carts/${this.cartId}/totals`)
-      .then((res) => res.data)
-      .catch(() => undefined);
+    super(cartId, true);
   }
 }
 
---
+++ src/services/magento/payable-cart.ts
@@ -0,0 +1,71 @@
+import { Cart, CartTotal } from '../../types/cart';
+import { Amount, Transaction } from 'paypal-rest-sdk';
+import fastify from '../../bootstrap/fastify';
+import PayPal from '../PayPal';
+import Magento from './index';
+import axios from 'axios';
+
+export default class PayableCart extends Magento {
+  private cartId: string;
+  public cart?: Cart;
+
+  constructor(cartId: string, isGuest: boolean) {
+    super();
+    this.axios = axios.create({
+      ...this.axios.defaults,
+      baseURL: `${process.env.MAGENTO_REST_URL}/${isGuest ? 'guest-carts' : 'carts'}`,
+    });
+    this.cartId = cartId;
+  }
+
+  async getCart(): Promise<Cart | undefined> {
+    return this.axios
+      .get<Cart>(this.cartId)
+      .then((res) => (this.cart = res.data))
+      .catch(() => (this.cart = undefined));
+  }
+
+  async getTotal(): Promise<CartTotal | undefined> {
+    return this.axios
+      .get<CartTotal>(`${this.cartId}/totals`)
+      .then((res) => res.data)
+      .catch(() => undefined);
+  }
+
+  async getPaymentLink(): Promise<string | undefined> {
+    const [cart, total] = await Promise.all([this.getCart(), this.getTotal()]);
+    if (!cart || !total) return undefined;
+
+    const shippingAddress = cart?.extension_attributes.shipping_assignments?.[0].shipping.address ?? cart.billing_address;
+    const ppItemList: Transaction['item_list'] = {
+      shipping_address: {
+        recipient_name: `${shippingAddress.firstname} ${shippingAddress.lastname}`,
+        phone: shippingAddress.telephone ?? '',
+        city: shippingAddress.city ?? '',
+        line1: shippingAddress.street.join(' '),
+        country_code: shippingAddress.country_id ?? '',
+        state: shippingAddress.region ?? '',
+        postal_code: shippingAddress.postcode ?? '',
+      },
+      items: cart.items.map((item) => ({
+        currency: total.base_currency_code,
+        name: item.name,
+        price: item.price.toFixed(2),
+        quantity: item.qty,
+        sku: item.sku,
+      })),
+    };
+    fastify.log.debug(`PayPal item_list: ${JSON.stringify(ppItemList)}`);
+    const ppAmount: Amount = {
+      currency: total.base_currency_code,
+      total: total.grand_total.toFixed(2),
+      details: {
+        subtotal: total.subtotal.toFixed(2),
+        shipping: total.shipping_amount.toFixed(2),
+      },
+    };
+    const paymentLink = await PayPal.getPaymentResponse(ppItemList, ppAmount);
+    fastify.log.info(`PayPal link for ${this.cartId} --> ${paymentLink}`);
+    return paymentLink;
+  }
+}
